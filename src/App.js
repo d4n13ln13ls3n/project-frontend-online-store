47fe2352af603cbc357005257618a86e2fd3bf17
Marina Andrade Catão <marinavdac@gmail.com>
2022-05-24 17:09:07 -0300

    requisito 03 concluido

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { Route, BrowserRouter } from 'react-router-dom';
 import ProductList from './pages/ProductList';
+import CartList from './pages/CartList';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Route path="/" component={ ProductList } />
+        <Route path="/cart-list" component={ CartList } />
       </BrowserRouter>
     </div>
   );
