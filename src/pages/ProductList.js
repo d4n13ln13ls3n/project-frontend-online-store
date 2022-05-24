e7575ad54a8f11aab0ae81fb62040f733be16a5a
Daniel Yabu <danielyabu@yahoo.com>
2022-05-24 16:03:20 -0300

    Requisito 2 concluido com lint revisado

---
+++ src/pages/ProductList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class ProductList extends React.Component {
+  render() {
+    return (
+      <span
+        data-testid="home-initial-message"
+      >
+        Digite algum termo de pesquisa ou escolha uma categoria.
+      </span>
+    );
+  }
+}
+
+export default ProductList;
